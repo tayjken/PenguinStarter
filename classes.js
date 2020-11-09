341a68c0d77b3a27358a6d2883f1156fa1ac82d6
Taylor Kennedy <taylor.kennedy@centre.edu>
2020-11-08 19:40:25 -0500

    sort changes

--- classes.js
+++ classes.js
@@ -4,7 +4,6 @@ var getIMG = function(image){
 }
 
 var quizMeans = function(penguin){
-    //array of quiz grades
     var studentQuiz = penguin.quizes.map(function(quiz){
         return quiz.grade;
     })
@@ -12,11 +11,9 @@ var quizMeans = function(penguin){
 }
 
 var hwMeans=function(penguin){
-    //map selects grades out of the group of objects in the hw array
     var studentHw = penguin.homework.map(function(hw){
         return hw.grade;
     })
-    //takes the hw grades and returns the mean
     return d3.mean(studentHw).toFixed(2)
 }
 var testMean = function(penguin){
@@ -31,36 +28,66 @@ var finalMean = function(penguin){
     })
     return d3.mean(studentFinal).toFixed(2)
 }
-var clearTable=function(){
+var clearTable = function(){
     d3.selectAll("tbody tr")
     .remove();
 }
-
-var successFCN = function(penguins){
-    console.log(penguins);
-    
-    var rows=d3.select("tbody")
+var drawTable = function(penguins){
+    var rows = d3.select(" tbody")
     .selectAll("tr")
     .data(penguins)
     .enter()
-    .append("tr")
+    .append("tr");
     
-    rows.append("td")
+     rows.append("td")
     .append("img")
+    .attr("class","pict")
     .attr("src",getIMG);
     
     rows.append("td")
+    .attr("class","quiz")
     .text(quizMeans);
     
     rows.append("td")
+    .attr("class","hw")
     .text(hwMeans);
     
     rows.append("td")
+    .attr("class","test")
     .text(testMean);
     
     rows.append("td")
     .attr("class","finalGrade")
     .text(finalMean);
+};
+
+var clearTab;e=function(){
+    d3.selectAll("tbody tr")
+    .remove();
+}
+
+var initHeaders = function(penguins){
+    d3.select("#finalGrade")
+    .on("click",function(){
+        console.log("clicked quiz");
+        penguins.sort(function(a,b) {
+            var av1 = finalMean(a);
+            var av2 = finalMean(b);
+            if (av1 < av2) {return 1;}
+            else if (av1 > av2){return -1;}
+            else {return 0;}
+        });
+        clearTable();
+        drawTable(penguins)
+        d3.selectAll(".finalGrade")
+        .attr("class","selected");
+    });
+}
+
+var successFCN = function(penguins){
+   console.log("data",penguins);
+    drawTable(penguins);
+    initHeaders(penguins)
 }
 
 var failFCN=function(errMessage){
